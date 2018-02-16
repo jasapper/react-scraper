a750445b93db813a1709915261d65873a73702ad
jasapper <jasap98@yahoo.com>
2018-02-16 00:53:19 -0500

    C-R-A commit, mongo added

---
+++ server.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const request = require("request"); 
+const cheerio = require("cheerio");
+const logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoReactHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI); // "useMongoClient" is deprecated
+
+var db = mongoose.connection;
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
