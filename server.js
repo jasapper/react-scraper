58efccae9a232fb7ddcfe6f62afe8219289cafcc
jasapper <jasap98@yahoo.com>
2018-02-16 01:37:14 -0500

    Attempt to fix build error on Heroku (path)

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var db = mongoose.connection;
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 }
 
 // Send every request to the React app
